d4e3dc630ab1cca3462d6d28556b4483b9ad68f8
Olivier Combe <olivier.combe@gmail.com>
2021-07-29 18:41:25 +0200

    fix test & lint for angular-v8

--- examples/demo-lib-v8/src/bit-test.spec.ts
+++ examples/demo-lib-v8/src/bit-test.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BitTestComponent } from './bit-test.component';
+import { BitTestService } from './bit-test.service';
 
 describe('BitTestComponent', () => {
   let component: BitTestComponent;
@@ -8,6 +9,7 @@ describe('BitTestComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      providers: [ BitTestService ],
       declarations: [ BitTestComponent ]
     })
     .compileComponents();
