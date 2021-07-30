990b119032c0d7f0614b4b8da70951b08923ea74
Olivier Combe <olivier.combe@gmail.com>
2021-07-30 15:48:25 +0200

    improve docs styles

--- packages/angular/preview/src/app/docs/docs.component.ts
+++ packages/angular/preview/src/app/docs/docs.component.ts
@@ -2,7 +2,7 @@ import { Component, Input, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-docs',
-  template: ` <div #docsRoot [innerHTML]="template | safeHtml"></div> `,
+  template: ` <div #docsRoot [innerHTML]="template | safeHtml" class='docsMainBlock'></div> `,
   styleUrls: ['./docs.style.scss'],
 })
 export class DocsComponent {
