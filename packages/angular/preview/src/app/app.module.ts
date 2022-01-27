b0bb155f4446e49b60c79922aa6ff3657b0dfe62
Olivier Combe <olivier.combe@gmail.com>
2022-01-27 11:25:02 +0100

    fix docs module compilation in angular-v12

--- packages/angular/preview/src/app/app.module.ts
+++ packages/angular/preview/src/app/app.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { DocsModule } from './docs/docs.module';
+import { DocsComponent } from './docs/docs.component';
+import { SafeHtmlPipe } from './docs/safe-html.pipe';
 import { LazyLoadComponent } from './lazy-load/lazy-load.component';
 
 @NgModule({
-  declarations: [AppComponent, LazyLoadComponent],
-  imports: [BrowserModule, DocsModule],
+  declarations: [AppComponent, LazyLoadComponent, SafeHtmlPipe, DocsComponent],
+  imports: [BrowserModule],
   bootstrap: [AppComponent]
 })
 export class AppModule {
