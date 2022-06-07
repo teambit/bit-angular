420205995586070638035f614cb3d01605e61fba
Olivier Combe <olivier.combe@gmail.com>
2022-06-07 09:44:10 +0200

    fix type error

--- aspects/dev-services/compiler/angular-elements/rollup/ngc-plugin/index.ts
+++ aspects/dev-services/compiler/angular-elements/rollup/ngc-plugin/index.ts
@@ -1,10 +1,9 @@
-import type { CompilerHost, CompilerOptions } from '@angular/compiler-cli';
+import type { CompilerHost, AngularCompilerOptions } from '@angular/compiler-cli';
 import { Logger } from '@teambit/logger';
 import { resolve } from 'path';
 import { Plugin } from 'rollup';
 import {
   CompilerHost as TsCompilerHost,
-  CompilerOptions as TsCompilerOptions,
   ModuleKind,
   ModuleResolutionKind,
   ScriptTarget
@@ -31,7 +30,7 @@ export async function ngcPlugin(options: Options, logger: Logger): Promise<Plugi
   const { target = 'es2018', rootDir, sourceMap = true } = options;
   const scriptTarget = ScriptTarget[Number(target.toLocaleUpperCase())] as any;
 
-  const opts: CompilerOptions & TsCompilerOptions = {
+  const opts = {
     target: scriptTarget,
     module: ModuleKind.ESNext,
     lib: [ 'dom', 'es2015', 'es2017', 'es2018', 'es2019' ],
@@ -43,7 +42,7 @@ export async function ngcPlugin(options: Options, logger: Logger): Promise<Plugi
     emitDecoratorMetadata: true,
     enableIvy: true,
     sourceMap
-  }
+  } as any as AngularCompilerOptions;
 
   return {
     name: 'ngc',
