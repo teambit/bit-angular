0455169a0e52fb794a77c66b4aaad57371b70af1
Olivier Combe <olivier.combe@gmail.com>
2022-09-15 14:37:21 +0200

    export `AngularV13Main` as type

--- aspects/versions/angular-v13/index.ts
+++ aspects/versions/angular-v13/index.ts
@@ -1,3 +1,3 @@
-export { AngularV13Main } from './angular-v13.main.runtime';
+export type { AngularV13Main } from './angular-v13.main.runtime';
 export { AngularV13Env } from './angular-v13.env';
 export { AngularV13Aspect, AngularV13Aspect as default } from './angular-v13.aspect';
