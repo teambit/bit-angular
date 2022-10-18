249709303374ae3f54c423f6c4ca5ff5be210e5c
Olivier Combe <olivier.combe@gmail.com>
2022-10-18 17:54:47 +0200

    fix `resolveId` for rollup ngc plugin

--- aspects/dev-services/compiler/angular-elements/rollup/ngc-plugin/resolver.ts
+++ aspects/dev-services/compiler/angular-elements/rollup/ngc-plugin/resolver.ts
@@ -1,26 +1,9 @@
-import { join, dirname } from 'path';
-import { existsSync } from 'fs';
-
 export function resolver(extensions?: string[]) {
-  const _extensions = ['ts', 'tsx', ...(extensions ?? [])];
-
-  const resolveFile = (resolved: string) => {
-    for (const extension of _extensions) {
-      const file = `${resolved}.${extension}`;
-      if (existsSync(file)) {
-        return file;
-      }
-    }
-  };
-
   return function resolveId(id: string, origin: string | undefined) {
     if (!origin || id.includes('node_modules')) {
       return id;
     }
-    const resolved = join(dirname(origin), id);
-    const file = resolveFile(resolved);
-    if (file) {
-      return file;
-    }
-  };
+    // anything else is to be considered external and not bundled
+    return false;
+  }
 }
