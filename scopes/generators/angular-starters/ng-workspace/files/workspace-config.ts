b565e81b707e71ccd6f3cb768f5ab49841795367
Olivier Combe <olivier.combe@gmail.com>
2023-04-18 17:01:27 +0200

    use pnpm & root components for the starter

--- scopes/generators/angular-starters/ng-workspace/files/workspace-config.ts
+++ scopes/generators/angular-starters/ng-workspace/files/workspace-config.ts
@@ -14,7 +14,8 @@ export async function workspaceConfig({ name, defaultScope, aspectComponent }: W
   const configParsed = await getWorkspaceConfigTemplateParsed();
   configParsed['teambit.workspace/workspace'].name = name;
   configParsed['teambit.workspace/workspace'].defaultScope = scope;
-  configParsed['teambit.dependencies/dependency-resolver'].packageManager = 'teambit.dependencies/yarn';
+  configParsed['teambit.dependencies/dependency-resolver'].packageManager = 'teambit.dependencies/pnpm';
+  configParsed['teambit.dependencies/dependency-resolver'].rootComponents = true;
   configParsed['teambit.dependencies/dependency-resolver'].policy = {
     "dependencies": {
       [envPkgName]: `^${envVersion}`
