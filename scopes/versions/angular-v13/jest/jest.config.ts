28a8df94ff29430ecf0b6d82a86aefaa4a379381
Olivier Combe <olivier.combe@gmail.com>
2023-05-02 15:11:08 +0200

    fix old jest configs to work with pnpm

--- scopes/versions/angular-v13/jest/jest.config.ts
+++ scopes/versions/angular-v13/jest/jest.config.ts
@@ -11,18 +11,6 @@ export default {
       tsconfig: require.resolve('./tsconfig.spec.json'),
     },
   },
-  moduleNameMapper: {
-    // map angular modules to the root node_modules to avoid duplicated modules
-    "(@angular\/animations.*)$": "<rootDir>/node_modules/$1",
-    "(@angular\/common.*)$": "<rootDir>/node_modules/$1",
-    "(@angular\/compiler.*)$": "<rootDir>/node_modules/$1",
-    "(@angular\/compiler-cli.*)$": "<rootDir>/node_modules/$1",
-    "(@angular\/core.*)$": "<rootDir>/node_modules/$1",
-    "(@angular\/forms.*)$": "<rootDir>/node_modules/$1",
-    "(@angular\/platform-browser.*)$": "<rootDir>/node_modules/$1",
-    "(@angular\/platform-browser-dynamic.*)$": "<rootDir>/node_modules/$1",
-    "(@angular\/router.*)$": "<rootDir>/node_modules/$1",
-  },
   transformIgnorePatterns: [
     '^.+.module.(css|sass|scss)$',
     generateNodeModulesPattern({
