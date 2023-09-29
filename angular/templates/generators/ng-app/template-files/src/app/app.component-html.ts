309079afd7c20b773c26c9c15c88c7054ef8e718
Olivier Combe <olivier.combe@gmail.com>
2023-09-29 15:28:10 +0200

    fix: default app template create cmp button

--- angular/templates/generators/ng-app/template-files/src/app/app.component-html.ts
+++ angular/templates/generators/ng-app/template-files/src/app/app.component-html.ts
@@ -404,7 +404,7 @@ export const appComponentHtmlFile = (): ComponentFile => {
 
     <button class="card card-small" (click)="selection.value = 'pwa'" tabindex="0">
       <svg class="material-icons" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"/></svg>
-      <span>Add PWA Support</span>
+      <span>Create component</span>
     </button>
 
     <button class="card card-small" (click)="selection.value = 'dependency'" tabindex="0">
